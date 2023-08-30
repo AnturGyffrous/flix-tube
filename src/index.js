368b444836a21654098b29149c6e8dddda88ec1b
Owain Williams <owain@hedfan.uk>
2023-08-31 00:01:14 +0100

    Update index.js to use a PORT environment varibale

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ const fs = require("fs");
  
 const app = express();
  
-const port = 3000;
- 
+if (!process.env.PORT) {
+  throw new Error("Environment variable PORT not found");
+}
+
+const PORT = process.env.PORT;
+
 app.get("/video", async (req, res) => {
   const videoPath = "videos/SampleVideo_1280x720_1mb.mp4";
   const stats = await fs.promises.stat(videoPath);
- 
+
   res.writeHead(200, {
     "Content-Length": stats.size,
     "Content-Type": "video/mp4",
-  });  
+  });
   fs.createReadStream(videoPath).pipe(res);
 });
- 
-app.listen(port);
+
+app.listen(PORT);
