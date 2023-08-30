643487760dada023318ee0e4b8a3b5bd6c38671c
Owain Williams <owain@hedfan.uk>
2023-08-30 23:51:07 +0100

    Update index.js to stream the video on the /video GET route

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
-
+const fs = require("fs");
+ 
 const app = express();
+ 
 const port = 3000;
-
-//
-// Registers a HTTP GET route.
-//
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+ 
+app.get("/video", async (req, res) => {
+  const videoPath = "videos/SampleVideo_1280x720_1mb.mp4";
+  const stats = await fs.promises.stat(videoPath);
+ 
+  res.writeHead(200, {
+    "Content-Length": stats.size,
+    "Content-Type": "video/mp4",
+  });  
+  fs.createReadStream(videoPath).pipe(res);
 });
-
-//
-// Starts the HTTP server.
-//
-app.listen(port, () => {
-    console.log(`First example listening on port ${port}, point your browser at http://localhost:${port}`);
-});
+ 
+app.listen(port);
